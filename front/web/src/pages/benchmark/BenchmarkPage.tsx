9a4d2866bc55463a1719a0928312da6120b18b7b
ANWAR MEQOR <anwar.meqor@doriane.com>
2026-02-24 10:36:18 +0100

    feat(front): benchmark page

--- front/web/src/pages/benchmark/BenchmarkPage.tsx
+++ front/web/src/pages/benchmark/BenchmarkPage.tsx
@@ -302,7 +302,7 @@ export const BenchmarkPage: React.FC = () => {
           {/* PROFIL PRATIQUES */}
           <PracticeProfileCard
             title={"Liste des pratiques les plus performantes sur l'IFT"}
-            items={practiceProfile.filter((item) => item.id !== 'agriculture-type')}
+            items={practiceProfile.filter((item) => item.id !== 'agriculture-type' && item.id !== 'departement')}
           />
         </>
       )}
